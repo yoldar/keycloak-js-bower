9dc22ca85a256ee2220f0ac95cd8d122b02e5f94
Eldar Zakiryanov <eldar.zakiryanov@gmail.com>
2018-03-12 14:57:07 +0600

    https://issues.jboss.org/browse/KEYCLOAK-5434?_sscc=t

--- dist/keycloak.js
+++ dist/keycloak.js
@@ -931,16 +931,19 @@
                         if (arguments.length == 1) {
                             encodeHash = true;
                         }
-
+ 
                         if (options && options.redirectUri) {
                             return options.redirectUri;
                         } else if (kc.redirectUri) {
                             return kc.redirectUri;
                         } else {
                             var redirectUri = location.href;
-                            if (location.hash && encodeHash) {
-                                redirectUri = redirectUri.substring(0, location.href.indexOf('#'));
-                                redirectUri += (redirectUri.indexOf('?') == -1 ? '?' : '&') + 'redirect_fragment=' + encodeURIComponent(location.hash.substring(1));
+                            var parts = redirectUri.split('?');
+                            var hasRedirectFragment = parts[1] && parts[1].substr(0, 'redirect_fragment'.length) === 'redirect_fragment';
+ 
+                            if (!hasRedirectFragment && location.hash && encodeHash) {
+                              redirectUri = redirectUri.substring(0, location.href.indexOf('#'));
+                              redirectUri += (redirectUri.indexOf('?') == -1 ? '?' : '&') + 'redirect_fragment=' + encodeURIComponent(location.hash.substring(1));
                             }
                             return redirectUri;
                         }
